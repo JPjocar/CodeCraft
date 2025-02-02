d2de42cafa1220babd1e927eebba83b5672bd934
Cosmos <jpjocar.6@gmail.com>
2025-02-02 04:51:28 -0500

    CodeCraft Project Start

---
+++ src/main/java/com/cosmos/CodeCraft/Controller/QuestionController.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cosmos.CodeCraft.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuestionController {
+    
+}
